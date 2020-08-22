765c2a3eb645d151d998ddf9adf32aaf75a7b894
Josiah Powell <josiahpowell@outlook.com>
2020-08-22 10:29:52 -0500

    updating server.js

--- server.js
+++ server.js
@@ -37,37 +37,13 @@ app.get("/:route", function (req, res) {
     }
 });
 
-app.get("/api/characters", function (req, res) {
-    return res.json(characters);
-});
-
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function (req, res) {
-    var chosen = req.params.character;
-
-    for (var i = 0; i < characters.length; i++) {
-        if (chosen === characters[i].routeName) {
-            return res.json(characters[i]);
-        }
-    }
-
-    return res.json(false);
-});
-
-
 function generatePage({ title, main }, done) {
     fs.readFile(__dirname + "/template.html", "utf8", (err, templateData) => {
         if (err)
             throw err;
 
-        let nav = "";
-        for (const item in data) {
-            if (data[item].navType !== "main") continue;
-            nav += `<div class="button is-3 is-large is-rounded mx-3 is-outlined"><a href="/${item}">${item.replace("-", " ")}</a></div>`;
-        }
         done(templateData.toString()
             .replace(new RegExp("{{ title }}", "gm"), title)
-            .replace(new RegExp("{{ nav }}", "gm"), nav)
             .replace(new RegExp("{{ main }}", "gm"), main));
 
     });
